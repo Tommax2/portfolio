f7a12a2dd459241914c2cc36a720d5c6762f5a28
Tommax2 <martinsolumi@gmail.com>
2026-01-09 13:22:15 +0100

    add

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ import { createTransport } from "nodemailer";
 const app = express();
 app.use(cors());
 app.use(json());
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
 
 
 const contactEmail = createTransport({
