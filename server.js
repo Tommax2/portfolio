353a87e87a85dbd9ca24fa0e1278a043f5379f37
Tommax2 <martinsolumi@gmail.com>
2026-01-16 18:13:05 +0100

    add

--- server.js
+++ server.js
@@ -33,14 +33,19 @@ app.get("/", (req, res) => {
 
 // Email configuration
 const contactEmail = createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
     tls: {
         rejectUnauthorized: false
-    }
+    },
+    connectionTimeout: 10000, // 10 seconds
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
 });
 
 contactEmail.verify((error) => {
